2840adf3d1d9b487b8bad3989cf45bbc43750cb1
Derek Willis <dwillis@gmail.com>
2023-01-06 12:33:53 -0500

    added team ranks

--- snapshot.sh
+++ snapshot.sh
@@ -3,3 +3,4 @@ sql2csv --db mysql://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME} --query "select
 sql2csv --db mysql://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME} --query "select * from stats_player_box_rollup_values_with_names where rollup like \"2023-ncaa%-natl\"" > "data/player_totals_${today}.csv"
 sql2csv --db mysql://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME} --query "select * from stats_game_box_player_with_names where end_year = 2023 and league = 'NCAA'" > "data/player_gameboxes_${today}.csv"
 sql2csv --db mysql://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME} --query "select * from stats_game_box_team_total where source = 'ncaa' and hhs_game_id_text IN (select hhs_game_id_text from game where league = 'NCAA' and end_year = 2023 order by gamedate_east desc)" > "data/team_gameboxes_${today}.csv"
+sql2csv --db mysql://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME} --query "select * from stats_team_box_rollup_ranks" > "data/team_ranks_${today}.csv"
